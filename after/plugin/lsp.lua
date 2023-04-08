6169d2013436cc50c80d93dc55d6f7162e778877
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-04-08 18:15:30 -0400

    remapped some lsp functions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -33,8 +33,12 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
 })
 
 local cmp_action = require('lsp-zero').cmp_action()
