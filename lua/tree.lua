66a095e4a6d16861542ed1bd9fb785b91e75e302
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-04-08 00:50:16 -0400

    cleaned tree start script

--- lua/tree.lua
+++ lua/tree.lua
@@ -1,6 +1,8 @@
 local function open_nvim_tree()
-    require("nvim-tree.api").tree.open()
-    require("nvim-tree.api").tree.toggle_hidden_filter()
+    local api = require("nvim-tree.api")
+
+    api.tree.open()
+    api.tree.toggle_hidden_filter()
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
