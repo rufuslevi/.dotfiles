6e6046ef3f6d987a8b21b5e9d85cf5ded46f9960
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-04-22 17:48:32 -0400

    adapted close button function call

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,25 +1,28 @@
 local tabbar = require("bufferline")
 
 tabbar.setup({
-	options = {
-		diagnostics = "coc",
-		diagnostics_update_in_insert = true,
-		offsets = {
-			{
-				filetype = "NvimTree",
-				text = "Nvim Tree",
-				text_align = "center",
-				separator = true,
-			},
-		},
-		color_icons = true,
-		separator_style = "thin",
-		hover = {
-			enabled = true,
-			delay = 0,
-			reveal = { "close" },
-		},
-	},
+    options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_update_in_insert = true,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "Nvim Tree",
+                text_align = "center",
+                separator = true,
+            },
+        },
+        color_icons = true,
+        separator_style = "thin",
+        hover = {
+            enabled = true,
+            delay = 0,
+            reveal = { "close" },
+        },
+        close_command = function(bufnum)
+            require('bufdelete').bufdelete(bufnum, true)
+        end
+    },
 })
 
 vim.keymap.set("n", "<leader>bd", vim.cmd.Bdelete)
