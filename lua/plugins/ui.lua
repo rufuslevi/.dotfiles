35fab5f0971c22f27d70fa933da98cd90018f8af
Michael Roussel <michaelroussel404@outlook.com>
2023-10-28 14:41:05 -0400

    duck update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -57,4 +57,15 @@ return {
       require("pretty_hover").setup(opts)
     end,
   },
+  {
+    "tamton-aquib/duck.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>dd", function()
+        require("duck").hatch()
+      end, {})
+      vim.keymap.set("n", "<leader>dk", function()
+        require("duck").cook()
+      end, {})
+    end,
+  },
 }
