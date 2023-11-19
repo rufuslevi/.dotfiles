a3f4bef536a61a62829bdbe99a5969aa0d431e52
Michael Roussel <michaelroussel404@outlook.com>
2023-11-19 16:51:00 -0500

    moved to dotfiles with symlinks

--- lua/config/keymaps/addons.lua
+++
@@ -1,3 +0,0 @@
--- This is where the keymaps for the addons are set
-
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
--- lua/config/keymaps/init.lua
+++
@@ -1,5 +0,0 @@
--- This is the central point where all keymaps are to be loaded
-
-require("config.keymaps.ui")
-require("config.keymaps.telescope")
-require("config.keymaps.addons")
