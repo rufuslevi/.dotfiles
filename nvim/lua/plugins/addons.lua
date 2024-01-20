3461f643e35368634e6fb3cee94f3d65fbe00591
Michael Roussel <michaelroussel404@outlook.com>
2024-01-20 13:23:52 -0500

    nvim/ Added support for anaconda environments

--- nvim/lua/plugins/addons.lua
+++ nvim/lua/plugins/addons.lua
@@ -54,4 +54,21 @@ return {
       })
     end,
   },
+  {
+    "dccsillag/magma-nvim",
+    run = "UpdateRemotePlugins",
+    keys = {
+      { "<leader>mi", "<cmd>MagmaInit<CR>", desc = "This command initializes a runtime for the current buffer." },
+      { "<leader>mo", "<cmd>MagmaEvaluateOperator<CR>", desc = "Evaluate the text given by some operator." },
+      { "<leader>ml", "<cmd>MagmaEvaluateLine<CR>", desc = "Evaluate the current line." },
+      { "<leader>mv", "<cmd>MagmaEvaluateVisual<CR>", desc = "Evaluate the selected text." },
+      { "<leader>mc", "<cmd>MagmaEvaluateOperator<CR>", desc = "Reevaluate the currently selected cell." },
+      { "<leader>mr", "<cmd>MagmaRestart!<CR>", desc = "Shuts down and restarts the current kernel." },
+      {
+        "<leader>mx",
+        "<cmd>MagmaInterrupt<CR>",
+        desc = "Interrupts the currently running cell and does nothing if not cell is running.",
+      },
+    },
+  },
 }
