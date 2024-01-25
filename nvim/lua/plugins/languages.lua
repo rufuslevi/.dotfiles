24b34e2f8e3754fcdddf636512700b18c8685542
Michael Roussel <michaelroussel404@outlook.com>
2024-01-24 23:05:28 -0500

    nvim/ added nulang support

--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -1,8 +1,10 @@
 return {
-  "LhKipp/nvim-nu",
-  config = function()
-    require("nu").setup({
-      use_lsp_features = true,
-    })
-  end,
+  {
+    "LhKipp/nvim-nu",
+    config = function()
+      require("nu").setup({
+        use_lsp_features = true,
+      })
+    end,
+  },
 }
