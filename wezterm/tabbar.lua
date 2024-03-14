b77e0f2b62afd0ac981d61cd0b9d579e4c151383
Michael Roussel <michaelroussel404@outlook.com>
2024-03-14 15:12:24 -0400

    wezterm/ Added tabbar catppuccin theme

---
+++ wezterm/tabbar.lua
@@ -0,0 +1,49 @@
+local SOLID_LEFT_ARROW = Wezterm.nerdfonts.ple_left_half_circle_thick
+local SOLID_RIGHT_ARROW = Wezterm.nerdfonts.ple_right_half_circle_thick
+
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  if title and #title > 0 then
+    return title
+  end
+  return tab_info.active_pane.title
+end
+
+Wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local edge_background = '#1e1e2e'
+    local background = '#181825'
+    local foreground = '#606479'
+
+    if tab.is_active then
+      background = '#181825'
+      foreground = '#ba99e3'
+    elseif hover then
+      background = '#181825'
+      foreground = '#ba99e3'
+    end
+
+    local edge_foreground = background
+
+    local title = tab_title(tab)
+
+    -- ensure that the titles fit in the available space,
+    -- and that we have room for the edges.
+    title = Wezterm.truncate_right(title, max_width - 2)
+
+    return {
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_LEFT_ARROW },
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = title },
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_RIGHT_ARROW },
+    }
+  end
+)
+
+return {}
