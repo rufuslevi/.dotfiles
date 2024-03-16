b82be212be5d2c0d369bd597ae827de73d7b1c09
Michael Roussel <michaelroussel404@outlook.com>
2024-03-16 12:36:09 -0400

    nixos/trying to export grub entries to new file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,14 +16,7 @@
       useOSProber = true;
       efiSupport = true;
       theme = "/boot/grub/themes/hyperfluent";
-      extraEntries = ''
-        menuentry 'Windows Boot Manager (the right one) (on /dev/nvme0n1p1)' --class windows --class os $menuentry_id_option 'osprober-efi-FAF9-831B' {
-          insmod part_gpt
-          insmod fat
-          search --no-floppy --fs-uuid --set=root FAF9-831B
-          chainloader /Microsoft/Boot/bootmgfw.efi
-        }
-      '';
+      extraEntries = import ./grub-entries.nix;
     };
   };
 
