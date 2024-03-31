94bc9627e75387de89f33a00d3b3964a8e318cbc
Michael Roussel <michaelroussel404@outlook.com>
2024-03-31 16:59:37 -0400

    nixos/more nvidia things

--- nixos/milkyway/configuration.nix
+++ nixos/milkyway/configuration.nix
@@ -70,9 +70,5 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    nvidia-x11
-    nvidia-settings
-    nvidia-persistenced
-  ];
+  environment.systemPackages = with pkgs; [ ];
 }
