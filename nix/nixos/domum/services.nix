834007dfb36c5e89082d9f93e2e3bc0b7ab643e8
Michael Roussel <michaelroussel404@outlook.com>
2024-04-20 20:28:58 -0400

    domum/enabled syncthing

--- nix/nixos/domum/services.nix
+++ nix/nixos/domum/services.nix
@@ -2,7 +2,16 @@
 
 {
   printing.enable = true;
+
   desktopManager.plasma6.enable = true;
+
   openssh.ports = [ 22 443 2222 7422 ];
+
   hardware.openrgb.enable = true;
+
+  syncthing = {
+    enable = true;
+    user = "rufuslevi";
+    configDir = "/home/rufuslevi/.config/syncthing";
+  };
 }
