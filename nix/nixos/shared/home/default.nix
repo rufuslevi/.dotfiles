536333f3a95e608aadffbf059ac21a6d48dabe10
Michael Roussel <michaelroussel404@outlook.com>
2024-08-02 18:06:23 -0400

    nix/shared - readded missing mime

--- nix/nixos/shared/home/default.nix
+++ nix/nixos/shared/home/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports = [ ./themes ./programs ./services ];
+  imports = [ ./themes ./programs ./services ../xdg/mime.nix ];
 
   home.username = "rufuslevi";
   home.homeDirectory = "/home/rufuslevi";
