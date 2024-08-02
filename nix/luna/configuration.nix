94abe830be967703462e643208529fea19944dca
rufuslevi <michaelroussel404@outlook.com>
2024-08-02 19:34:20 -0400

    nix/luna - some finder tweaks

--- nix/luna/configuration.nix
+++ nix/luna/configuration.nix
@@ -29,6 +29,13 @@
     defaults = { 
       NSGlobalDomain.InitialKeyRepeat = 15;
       NSGlobalDomain.KeyRepeat = 2;
+      finder = {
+        FXDefaultSearchScope = "SCcf";
+        FXPreferredViewStyle = "Nlsv";
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        _FXShowPosixPathInTitle = false;
+      };
     };
   };
 
