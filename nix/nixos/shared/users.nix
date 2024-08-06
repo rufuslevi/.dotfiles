b524b66a78694e46e26fd1e5c772f8a27fea858c
rufuslevi <michaelroussel404@outlook.com>
2024-08-06 13:08:50 -0400

    nix/milkyway - update

--- nix/nixos/shared/users.nix
+++ nix/nixos/shared/users.nix
@@ -1,10 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   users = {
     users.rufuslevi = {
       createHome = true;
       homeMode = "750";
+      shell = pkgs.zsh;
       isNormalUser = true;
       description = "Michael Roussel";
       extraGroups = [ "networkmanager" "wheel" "video" "render" ];
