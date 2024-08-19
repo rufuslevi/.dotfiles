f7ced29e758c02bde69b5101f1304d276670deed
Michael Roussel <michaelroussel404@outlook.com>
2024-08-19 00:51:40 -0400

    nix/nixos - moved security to own file

--- nix/nixos/shared/default.nix
+++ nix/nixos/shared/default.nix
@@ -13,6 +13,7 @@
     ./packages
     ./locales.nix
     ./networking.nix
+    ./security.nix
     ./users.nix
   ];
 
@@ -23,27 +24,6 @@
   ];
 
   console.keyMap = "cf";
-
-  security = {
-    rtkit.enable = true;
-    polkit.enable = true;
-  };
-
-  systemd = {
-    user.services.polkit-kde-authentication-agent-1 = {
-      description = "polkit-kde-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
 
   fonts.packages = with pkgs; [
     (nerdfonts.override {
