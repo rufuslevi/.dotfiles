c506543ef9861d61f564818bc5b4d2a23cbb0821
Michael Roussel <michaelroussel404@outlook.com>
2024-08-19 19:01:28 -0400

    nvim/moved debug plugins to own file

---
+++ nvim/lua/plugins/debug.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      dap.adapters.godot = {
+        type = "server",
+        host = "127.0.0.1",
+        port = 6006,
+      }
+
+      dap.configurations.gdscript = {
+        {
+          type = "godot",
+          request = "launch",
+          name = "Launch scene",
+          project = "${workspaceFolder}",
+          launch_scene = true,
+        },
+      }
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end,
+  },
+}
--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -14,27 +14,6 @@ return {
     end,
   },
   {
-    "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-      dap.adapters.godot = {
-        type = "server",
-        host = "127.0.0.1",
-        port = 6006,
-      }
-
-      dap.configurations.gdscript = {
-        {
-          type = "godot",
-          request = "launch",
-          name = "Launch scene",
-          project = "${workspaceFolder}",
-          launch_scene = true,
-        },
-      }
-    end,
-  },
-  {
     "ray-x/go.nvim",
     dependencies = { -- optional packages
       "ray-x/guihua.lua",
