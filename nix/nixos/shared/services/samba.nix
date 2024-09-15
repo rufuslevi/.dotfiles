0907de9845a07e4d2238f9ca382c6e5fd387da11
Michael Roussel <michaelroussel404@outlook.com>
2024-09-14 23:17:55 -0400

    nixos/shared - updated samba config

--- nix/nixos/shared/services/samba.nix
+++ nix/nixos/shared/services/samba.nix
@@ -5,17 +5,20 @@
     enable = true;
     securityType = "user";
     openFirewall = true;
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = smbnix
-      netbios name = smbnix
-      security = user 
-      securityType = "user"
-      hosts allow = 192.168.0. 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
-      guest account = rufuslevi
-      map to guest = bad user
-    '';
+    settings = {
+
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        "securityType" = "user";
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "rufuslevi";
+        "map to guest" = "bad user";
+      };
+    };
   };
   services.samba-wsdd = {
     enable = true;
