630d05c81a90410e15d635ed13d189584278d9c4
rufuslevi <michaelroussel404@outlook.com>
2025-03-10 16:03:30 -0400

    nvim/added toml languages support

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.tex" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
