ae0fc19dbe337df30f51154696868298494a199f
rufuslevi <michaelroussel404@outlook.com>
2025-03-11 12:57:47 -0400

    scripts/fixed razer keyboard keymaps

--- scripts/razer-keymapping.sh
+++ scripts/razer-keymapping.sh
@@ -2,26 +2,18 @@
 
 # Razer Huntsman V2 Menu to Right Option
 /usr/bin/hidutil property \
-    --matching '{"ProductID":0x026b}' \
-    --set '{"UserKeyMapping":[
-          {
-            "HIDKeyboardModifierMappingSrc": 0x7000000E3,
-            "HIDKeyboardModifierMappingDst": 0x7000000E2
-          },
-          {
-            "HIDKeyboardModifierMappingSrc": 0x7000000E2,
-            "HIDKeyboardModifierMappingDst": 0x7000000E3
-          },
+  --matching '{"ProductID":0x026b}' \
+  --set '{"UserKeyMapping":[
           {
             "HIDKeyboardModifierMappingSrc": 0x700000035,
             "HIDKeyboardModifierMappingDst": 0x700000064
           },
           {
-            "HIDKeyboardModifierMappingSrc": 0x700000065,
-            "HIDKeyboardModifierMappingDst": 0x7000000E9
+            "HIDKeyboardModifierMappingSrc": 0x7000000E7,
+            "HIDKeyboardModifierMappingDst": 0x7000000E6
           },
           {
-            "HIDKeyboardModifierMappingSrc": 0x70000006E,
+            "HIDKeyboardModifierMappingSrc": 0x700000065,
             "HIDKeyboardModifierMappingDst": 0x7000000E7
-          },
-    ]}'
+          }
+  ]}'
