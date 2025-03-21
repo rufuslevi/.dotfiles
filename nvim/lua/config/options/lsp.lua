17cb6d18be441206823198c13151413d3d01957d
Michael Roussel <michaelroussel404@outlook.com>
2025-03-21 00:22:25 -0400

    nvim/changed python lsp

--- nvim/lua/config/options/lsp.lua
+++ nvim/lua/config/options/lsp.lua
@@ -2,4 +2,4 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
   update_in_insert = true,
   virtual_text = true,
 })
-vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_ruff = "ruff"
