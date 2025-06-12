9e1b67f3e993f5a6e3e59ce4e27280ee7e59480e
rufuslevi <michaelroussel404@outlook.com>
2025-06-12 12:32:38 -0400

    updated the keyboard keymaps for macos

--- scripts/undertale-keymapping.sh
+++ scripts/undertale-keymapping.sh
@@ -16,4 +16,23 @@
             "HIDKeyboardModifierMappingSrc": 0x700000065,
             "HIDKeyboardModifierMappingDst": 0x7000000E7
           },
+          {
+            "HIDKeyboardModifierMappingSrc": 0x70000001A,
+            "HIDKeyboardModifierMappingDst": 0x700000052
+          },
+
+          {
+            "HIDKeyboardModifierMappingSrc": 0x700000016,
+            "HIDKeyboardModifierMappingDst": 0x700000051
+          },
+
+          {
+            "HIDKeyboardModifierMappingSrc": 0x700000004,
+            "HIDKeyboardModifierMappingDst": 0x700000050
+          },
+
+          {
+            "HIDKeyboardModifierMappingSrc": 0x700000007,
+            "HIDKeyboardModifierMappingDst": 0x70000004F
+          } 
   ]}'
--- scripts/undo-undertale-keymapping.sh
+++ scripts/undo-undertale-keymapping.sh
@@ -16,4 +16,23 @@
             "HIDKeyboardModifierMappingSrc": 0x700000065,
             "HIDKeyboardModifierMappingDst": 0x7000000E7
           },
+          {
+            "HIDKeyboardModifierMappingSrc": 0x700000052,
+            "HIDKeyboardModifierMappingDst": 0x70000001A
+          },
+
+          {
+            "HIDKeyboardModifierMappingSrc": 0x700000051,
+            "HIDKeyboardModifierMappingDst": 0x700000016
+          },
+
+          {
+            "HIDKeyboardModifierMappingSrc": 0x700000050,
+            "HIDKeyboardModifierMappingDst": 0x700000004
+          },
+
+          {
+            "HIDKeyboardModifierMappingSrc": 0x70000004F,
+            "HIDKeyboardModifierMappingDst": 0x700000007
+          } 
   ]}'
