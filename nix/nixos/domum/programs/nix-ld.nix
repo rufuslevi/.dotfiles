98a57f6f5c327e91f814100296b7220d0f30af9f
Michael Roussel <michaelroussel404@outlook.com>
2025-07-10 19:29:31 -0400

    nixos/domum - added nix-ld dependencies

--- nix/nixos/domum/programs/nix-ld.nix
+++ nix/nixos/domum/programs/nix-ld.nix
@@ -2,14 +2,17 @@
 
 {
   programs.nix-ld.libraries = with pkgs; [
-    libsForQt5.qt5.qtgraphicaleffects
     qtcreator
-    qt5.full
     qt6.full
-    libsForQt5.full
     kdePackages.qt5compat
     kdePackages.qtdeclarative
+    libsForQt5.qt5.qtgraphicaleffects
+    sdl2-compat
+    sdl3
     pkgs-stable.tbb_2022_0
+    wayland
+    wayland-protocols
+    wayland-scanner
     xorg.libXcursor
     xorg.libXext
     xorg.libXxf86vm
