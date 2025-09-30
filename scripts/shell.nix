6004f7f9d1fc231eaa93ab1020916ea05c446b89
Michael Roussel <michaelroussel404@outlook.com>
2025-09-30 19:15:11 -0400

    scripts/added cmake variables in dev shell

--- scripts/shell.nix
+++ scripts/shell.nix
@@ -4,5 +4,7 @@ stdenv.mkDerivation {
   shellHook = ''
     echo "Created dev env and linked nix_ld"
     export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+    export CMAKE_C_COMPILER=${pkgs.gcc}/bin/gcc
+    export CMAKE_CXX_COMPILER=${pkgs.clang}/bin/gcc
   '';
 }
