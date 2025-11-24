42cd4dd074c9afa6854a36b7f37000ff8ce7a0c8
Michael Roussel <michaelroussel404@outlook.com>
2025-11-23 22:41:04 -0500

    nixos/domum - changed hyprland config and added condition

--- nix/nixos/domum/programs/hyprland.nix
+++ nix/nixos/domum/programs/hyprland.nix
@@ -15,12 +15,10 @@
   };
 
   programs.hyprland = {
-    enable = lib.mkIf (config.options.hyprlandEnabled);
+    enable = config.options.hyprlandEnabled;
     withUWSM = false;
     xwayland.enable = true;
     package = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    # package = pkgs.hyprland;
-    # portalPackage = pkgs.xdg-desktop-portal-hyprland;
   };
 }
